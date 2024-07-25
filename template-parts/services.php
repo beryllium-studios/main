6741e3196dd690a4f8ce316b0dcb3267c92e8277
lloan alas <lloanalas@outlook.com>
2024-07-24 23:20:49 -0700

    add new services section

---
+++ template-parts/services.php
@@ -0,0 +1,43 @@
+<!--Skill section start-->
+<section class="services-section" id="services-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-lg-12 text-center text-lg-right">
+				<h1 class="services-section-header">Services</h1>
+				<div class="skill-details">
+
+					<div class="row no-gutters services-area">
+						<div class="col-12 col-md-4 services text-center">
+							<div class="service-card wow fadeInRight">
+								<div class="icon-holder">
+									<i class="lni lni-briefcase"></i>
+								</div>
+								<h4 class="card-heading">Make Business</h4>
+								<p>Your success is our mission. We provide the tools to streamline and grow your business effortlessly.</p>
+							</div>
+						</div>
+						<div class="col-12 col-md-4 services text-center">
+							<div class="service-card wow fadeInRight">
+								<div class="icon-holder">
+									<i class="lni lni-bulb"></i>
+								</div>
+								<h4 class="card-heading">Innovative Ideas</h4>
+								<p>Fuel your business with our cutting-edge solutions and innovative strategies.</p>
+							</div>
+						</div>
+						<div class="col-12 col-md-4 services text-center">
+							<div class="service-card wow fadeInRight" data-wow-delay=".5s">
+								<div class="icon-holder">
+									<i class="lni lni-heart"></i>
+								</div>
+								<h4 class="card-heading">Our Hardwork</h4>
+								<p>We are dedicated to delivering reliable, high-quality solutions tailored to your needs.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Skill section start-->
