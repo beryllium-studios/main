4f89be90f7e727f5452493f83b1b0dd8b9a4dd0a
lloan alas <lloanalas@outlook.com>
2024-07-31 04:45:31 -0700

    update contact section

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -4,21 +4,22 @@
 		<div class="row">
 			<div class="col-12 col-lg-7">
 				<h4 class="heading text-center text-lg-left">GET IN TOUCH</h4>
-				<form class="row contact-form wow fadeInLeft" id="contact-form-data">
-					<div class="col-sm-12" id="result"></div>
-					<div class="col-12 col-md-5">
-						<input type="text" name="userName" placeholder="Your Name" class="form-control">
-						<input type="email" name="userEmail" placeholder="Email Address *" class="form-control">
-						<input type="text" name="userSubject" placeholder="Subject" class="form-control">
-					</div>
-					<div class="col-12 col-md-7">
-						<textarea class="form-control" name="userMessage" rows="6" placeholder="Your Message"></textarea>
-					</div>
-					<div class="col-12">
-						<a href="javascript:void(0);" class="btn yellow-and-black-btn rounded-pill w-100 contact_btn"><i class="fa fa-spinner fa-spin mr-2 d-none" aria-hidden="true"></i> <b>Send Message</b>
-						</a>
-					</div>
-				</form>
+				<?php echo do_shortcode('[contact-form-7 id="5ad8da0" title="Contact form 1"]'); ?>
+<!--				<form class="row contact-form wow fadeInLeft" id="contact-form-data">-->
+<!--					<div class="col-sm-12" id="result"></div>-->
+<!--					<div class="col-12 col-md-5">-->
+<!--						<input type="text" name="userName" placeholder="Your Name" class="form-control">-->
+<!--						<input type="email" name="userEmail" placeholder="Email Address *" class="form-control">-->
+<!--						<input type="text" name="userSubject" placeholder="Subject" class="form-control">-->
+<!--					</div>-->
+<!--					<div class="col-12 col-md-7">-->
+<!--						<textarea class="form-control" name="userMessage" rows="6" placeholder="Your Message"></textarea>-->
+<!--					</div>-->
+<!--					<div class="col-12">-->
+<!--						<a href="javascript:void(0);" class="btn yellow-and-black-btn rounded-pill w-100 contact_btn"><i class="fa fa-spinner fa-spin mr-2 d-none" aria-hidden="true"></i> <b>Send Message</b>-->
+<!--						</a>-->
+<!--					</div>-->
+<!--				</form>-->
 			</div>
 			<div class="col-12 col-lg-5 text-center text-lg-left position-relative">
 				<div class="contact-details wow fadeInRight">
